c459c0762d68c515a1052ae09e1151315bc6095d
Catekaze <longnvh2110@Gmail.com>
2023-10-28 14:22:36 +0700

    fix event drop rate

--- assets/js/dungeon.js
+++ assets/js/dungeon.js
@@ -275,7 +275,7 @@ const dungeonEvent = () => {
                         } else {
                             player.gold -= cost;
                             sfxConfirm.play();
-                            eventRoll = randomizeNum(1, 50);
+                            eventRoll = randomizeNum(1, 35);
                             if (eventRoll == 1) {
                                 createRelicReplicaPrint("shop");
                                 addDungeonMessage(`<span class="Heirloom">Congratulation, you hit the jackpot!!!! Now get out.</span>`);
